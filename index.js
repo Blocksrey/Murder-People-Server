5022dd1982482bdc8407c5f3b7b668ca80fc4c8a
blocksrocks <minersneedcoolshoes@gmail.com>
2020-03-30 03:24:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-	res.send("Welcome to NodeJS App on Heroku");
+	res.send(```
+		<!DOCTYPE html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+			</head>
+			<body>
+				Welcome to NodeJS App on Heroku
+			</body>
+		</html>
+	```);
 });
 
 app.listen(port);
