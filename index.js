b6987d8ce7e75830cca80bdb3ac9e21b3813798a
Jeff <jeff@blocks.rocks>
2021-03-02 00:41:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+var playerTimes = {};
+
 function receive(request, response) {
-	console.log(request);
+	playerTimes[request.ip] = 8;
+
+	if (!playerTimes[request.ip])
+		console.log("New address: " + request.ip);
 
 	response.setHeader("Content-Type", "application/json");
 
@@ -14,3 +19,13 @@ app.get("/", receive);
 app.listen(port);
 
 console.log("Hosted on port: " + port);
+
+
+
+
+
+function update() {
+
+}
+
+setInterval(update, 1000);
