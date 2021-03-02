c24db246bb563b4d3d2bdac271bb4e848ac85502
Jeff <jeff@blocks.rocks>
2021-03-01 22:40:51 -0800

    lmfao

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
 	res.send('<!DOCTYPE html><head><meta charset="UTF-8"><meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript"></head><body>Welcome to NodeJS App on Heroku</body></html>');
 });
 
