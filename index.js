d5ac2d149f44f315cd6e0791150c5ebb54deddb1
Jeff <jeff@blocks.rocks>
2021-03-02 00:12:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 function receive(request, response) {
+	console.log(request);
+
 	response.setHeader("Content-Type", "application/json");
+	
 	response.send("WTF");
 }
 
@@ -11,20 +14,3 @@ app.get("/", receive);
 app.listen(port);
 
 console.log("Hosted on port: " + port);
-
-/*
-app.get("/", function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send('<!DOCTYPE html><head><meta charset="UTF-8"><meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript"></head><body>Welcome to NodeJS App on Heroku</body></html>');
-});
-
-app.listen(port);
-*/
-
-/*
-function debug() {
-	console.log("Debug?");
-}
-
-setInterval(debug, 1000);
-*/
