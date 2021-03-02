4d83d50015edcf7c8a0239f37b93fe6c8081892d
Jeff <jeff@blocks.rocks>
2021-03-01 23:23:27 -0800

    jkjk

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require("http");
 
 function receive(request, res) {
+	console.log("ASDasd");
+
 	res.writeHead(200, {"Content-Type": "application/json"});
 	res.write("ASD");
 	res.end();
