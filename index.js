ff50b3ab13e1f401a62630aa61a70e404d8b96d8
Jeff <jeff@blocks.rocks>
2021-03-01 23:36:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ app.get("/asd", function(request, response) {
 	response.writeHead(200, {"Content-Type": "application/json"});
     response.send(JSON.stringify({message: 'Got the email'}));
 });
+
+function debug() {
+	console.log("Debug?");
+}
+
+setInterval(debug, 1000);
