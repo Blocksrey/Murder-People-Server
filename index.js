0478e2e46cd5a50ec8b30dcf6bd4990ac0823c1d
Jeff <jeff@blocks.rocks>
2021-03-01 23:51:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var app = express();
 
+/*
 function receive(request, response) {
 	console.log("WOAHA");
 
 }
 
 app.get("/", receive);
+*/
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
 
 function debug() {
 	console.log("Debug?");
