05622d9223abdc663b5bd3a70214aa1273ab7141
Jeff <jeff@blocks.rocks>
2021-03-02 00:02:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 var express = require("express");
 var app = express();
 
-/*
 function receive(request, response) {
-	console.log("WOAHA");
-
+	response.setHeader("Content-Type", "application/json");
+	res.send("THIS IS A RESPONSE!!!!!!!!!");
 }
-
-app.get("/", receive);
-*/
-
-
-app.get("/", function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send('<!DOCTYPE html><head><meta charset="UTF-8"><meta name="keywords" content="HTML, CSS, XML, XHTML, JavaScript"></head><body>Welcome to NodeJS App on Heroku</body></html>');
-});
 
 app.listen(8080);
 
-
+/*
 function debug() {
 	console.log("Debug?");
 }
 
 setInterval(debug, 1000);
+*/
